2954b7bdfe788e92fcb1b5e4dc4dde9a6c631c3a
IceBear8965 <icebee088@gmail.com>
2025-08-24 14:11:32 +0200

    Changed catppuccin flavour

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,22 +1,22 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  lazy = false,
-  priority = 1000,
-  opts = {
-    flavour = "macchiato", -- latte, frappe, macchiato, mocha
-    transparent_background = false,
-    integrations = {
-      treesitter = true,
-      telescope = true,
-      mason = true,
-      cmp = true,
-      gitsigns = true,
-      nvimtree = true,
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    opts = {
+        flavour = "frappe", -- latte, frappe, macchiato, mocha
+        transparent_background = false,
+        integrations = {
+            treesitter = true,
+            telescope = true,
+            mason = true,
+            cmp = true,
+            gitsigns = true,
+            nvimtree = true,
+        },
     },
-  },
-  config = function(_, opts)
-    require("catppuccin").setup(opts)
-    vim.cmd.colorscheme("catppuccin")
-  end,
+    config = function(_, opts)
+        require("catppuccin").setup(opts)
+        vim.cmd.colorscheme "catppuccin"
+    end,
 }
